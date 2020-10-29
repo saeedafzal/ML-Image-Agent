42453269aa49ce0dc30f4c77e1813335e7541986
Saeed Afzal <saeed.afzal@ada.ac.uk>
2020-10-29 22:58:47 +0000

    can have multiple models

--- agent.js
+++ agent.js
@@ -1,5 +1,17 @@
-const model = "MobileNet";
-const maleOrFemaleModel = "https://teachablemachine.withgoogle.com/models/Vsqm0_v-a/model.json";
+// To add a model, add an object with the name and url of the model in this list.
+const models = [
+    {
+        "name": "Male / Female",
+        "model": "https://teachablemachine.withgoogle.com/models/Vsqm0_v-a/model.json",
+        "first": true
+    },
+    {
+        "name": "MobileNet",
+        "model": "MobileNet"
+    }
+];
+
+let buttons = [], classifier;
 
 function id(id) {
     return document.getElementById(id);
@@ -30,19 +42,40 @@ function calculate() {
     // Display a placeholder message
     id("table").innerHTML = "";
     id("result-text").innerHTML = "Please wait...";
-    ml5.imageClassifier(maleOrFemaleModel).then(classifier => {
+    classifier.then(classifier => {
         classifier.classify(id("image"), displayResults);
     });
 }
 
-// Event listener to trigger the model when an image is uploaded
-id("file-input").onchange = e => {
-    const fileList = e.target.files;
-    if (fileList && fileList[0]) {
-        const reader = new FileReader();
-        reader.onload = e => id("image").src = e.target.result;
-        reader.readAsDataURL(fileList[0]);
+// Runs when the page loads
+document.addEventListener("DOMContentLoaded", () => {
+    // Create buttons for the model
+    models.forEach(model => {
+        const button = document.createElement("button");
+        button.innerHTML = model.name;
+        button.onclick = () => {
+            buttons.forEach(btn => btn.disabled = false);
+            button.disabled = true;
+            classifier = ml5.imageClassifier(model.model);
+        };
+        buttons.push(button);
+        id("model-btns").append(button);
+    });
 
-        calculate();
-    }
-};
+    // The default button is the first one in the list
+    buttons[0].disabled = true;
+    // Create classifier for first model in the list (as the button is not clicked to trigger it)
+    classifier = ml5.imageClassifier(models[0].model);
+
+    // Event listener to trigger the model when an image is uploaded
+    id("file-input").onchange = e => {
+        const fileList = e.target.files;
+        if (fileList && fileList[0]) {
+            const reader = new FileReader();
+            reader.onload = e => id("image").src = e.target.result;
+            reader.readAsDataURL(fileList[0]);
+
+            calculate();
+        }
+    };
+});
